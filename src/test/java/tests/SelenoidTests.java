16bbea4779bdf42482db0a1d0e3f3a31bc10776b
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-03-22 20:54:22 +0700

    SelenoidTests

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -1,8 +1,16 @@
 package tests;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SelenoidTests {
     //REST Assured doc:
@@ -27,5 +35,74 @@ public class SelenoidTests {
                 .get("https://selenoid.autotests.cloud/status")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    void successWithoutGivenWhenStatusTest() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void successStatusWithBasicAuthTest() {
+        given()
+                .auth().basic("user1","1234")
+                .when()
+                .get("https://selenoid.autotests.cloud:4444/wd/hub/status")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void successStatusResponceWithLogTest() {
+        given()
+                .auth().basic("user1","1234")
+                .when()
+                .get("https://selenoid.autotests.cloud:4444/wd/hub/status")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    void successStatusReadyTest() {
+        given()
+                .auth().basic("user1","1234")
+                .when()
+                .get("https://selenoid.autotests.cloud:4444/wd/hub/status")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+
+    @Test
+    void successStatusReadyWithAssertThatTest() {
+        Boolean result = given()
+                .auth().basic("user1","1234")
+                .when()
+                .get("https://selenoid.autotests.cloud:4444/wd/hub/status")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .extract()
+                .path("value.ready");
+
+        assertThat(result, is(true));
+    }
+
+    @Test
+    void successStatusReadyWithAssertThat1Test() {
+        ExtractableResponse<Response> result = given()
+                .auth().basic("user1","1234")
+                .when()
+                .get("https://selenoid.autotests.cloud:4444/wd/hub/status")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .extract();
+
+        assertThat(result.path("value.ready"), is(true));
     }
 }
